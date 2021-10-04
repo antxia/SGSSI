5916d5ec419d242be6a688fc7ec1091061b61036
antxia <72552698+antxia@users.noreply.github.com>
2021-10-04 16:12:12 +0200

    Add files via upload

---
+++ Lab03_2.3.py
@@ -0,0 +1,32 @@
+import hashlib, sys
+import difflib
+
+#Códigos de colores.
+AMARILLO = '\033[93m'
+ROJO = '\033[91m'
+BLANCO = '\033[0m'
+
+try:
+	#Coge el archivo leyendo del argumento pasado.
+	with open(sys.argv[1], 'rb') as f:
+		archivo1 = f.read()
+		#Calcula el resumen
+		resumen1 = str(hashlib.sha256(archivo1).hexdigest())
+	with open(sys.argv[2], 'rb') as f:
+		archivo2 = f.read()
+		#Calcula el resumen
+		resumen2 = str(hashlib.sha256(archivo2).hexdigest())
+#Si el fichero no existe devuelve un error.
+except Exception:
+	print(ROJO + "No se ha podido encontrar el fichero\n" + BLANCO)
+	exit(1)
+with open(sys.argv[1], 'r') as f:
+	file_1_text = f.readlines()
+with open(sys.argv[2], 'r') as f:
+	file_2_text = f.readlines()
+for line in difflib.unified_diff(file_1_text, file_2_text, fromfile=sys.argv[1], tofile=sys.argv[2], lineterm=''):
+    if line[0]=='+' and line[1]!='+':
+    	if line[1:-1]==str(resumen1) or line[1:-1]==str(resumen2):
+    		print(AMARILLO + "Los archivos cumplen con la condición" + BLANCO)
+    		exit(1)
+print(AMARILLO + "Los archivos no cumplen con la condición" + BLANCO)
---
+++ Lab04_1.3.py
@@ -0,0 +1,29 @@
+import hashlib, sys, shutil
+import random, string
+
+#Códigos de colores.
+AMARILLO = '\033[93m'
+ROJO = '\033[91m'
+BLANCO = '\033[0m'
+
+try:
+	original=r"{}".format(sys.argv[1])
+	copia=r"{}HEX.txt".format(sys.argv[1][:-4])
+	while True:
+		shutil.copyfile(original,copia)
+		with open("{}HEX.txt".format(sys.argv[1][:-4]), 'a') as f:
+			#Generar una secuencia aleatoria
+			sec=''.join(random.choice('abcdef0123456789') for _ in range(6))
+			#Guarda el documento con la línea adicional.
+			f.write("{} {}\n".format(sec,"JANT33"))
+		with open("{}HEX.txt".format(sys.argv[1][:-4]), 'rb') as f2:
+			archivo1 = f2.read()
+			#Calcula el resumen
+			resumen = str(hashlib.sha256(archivo1).hexdigest())
+		if resumen.startswith("0000"):
+			print(AMARILLO + "El documento {} se ha actualizado.".format(sys.argv[1])+BLANCO)
+			exit(1)		
+#Si el fichero no existe devuelve un error.
+except Exception:
+	print(ROJO + "No se ha podido encontrar el fichero\n" + BLANCO)
+	exit(1)
